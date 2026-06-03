0e0344855a620f0fd6763f92a14260d5312ae066
xDullingHoldVape <tomik25.tk@gmail.com>
2026-06-03 23:00:45 +0300

    Created Generator and Validator

---
+++ Exam/BFGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+namespace C_FinalTask
+{
+
+    // Generates all possible password combinations
+    public class BFGenerator
+    {
+        // Characters that can be used in generated passwords
+        public const string CHARSET = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+        private readonly int _maxLength;
+
+        public BFGenerator(int maxLength = 6)
+        {
+            _maxLength = maxLength;
+        }
+
+        // Generates all combinations of a given lengthб example: aa, ab, ac, ..., 99
+        public IEnumerable<string> GetCombinations(int length)
+        {
+            // Stores the current position for each character
+            int[] indices = new int[length];
+
+            while (true)
+            {
+                // Build the current combination
+                var chars = new char[length];
+                for (int i = 0; i < length; i++)
+                    chars[i] = CHARSET[indices[i]];
+
+                yield return new string(chars);
+
+                // Move to the next combination
+                int position = length - 1;
+                while (position >= 0)
+                {
+                    indices[position]++;
+                    if (indices[position] < CHARSET.Length)
+                        break; // No carry needed, stop
+
+                    // Reset current position and move left
+                    indices[position] = 0;
+                    position--;
+                }
+
+                // No more combinations left
+                if (position < 0)
+                    yield break;
+            }
+        }
+
+        // Calculates the total number of combinations, used for the progress bar.
+        public long TotalCombinations()
+        {
+            long total = 0;
+            long count = CHARSET.Length;
+            for (int len = 1; len <= _maxLength; len++)
+            {
+                total += count;
+                count *= CHARSET.Length;
+            }
+            return total;
+        }
+    }
+}
---
+++ Exam/BFValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace C_FinalTask
+{
+    // Validates generated passwords
+
+    public class BFValidator
+    {
+        // The hash we are trying to crack (set once at the start of the attack)
+        private readonly string _targetHash;
+
+        public BFValidator(string targetHash)
+        {
+            _targetHash = targetHash;
+        }
+
+        // Returns true if the password matches the target hash
+        public bool IsMatch(string candidate)
+        {
+            string candidateHash = PasswordManager.HashPassword(candidate);
+            return candidateHash == _targetHash;
+        }
+    }
+}
